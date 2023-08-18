3bcecb778c40eb816e22d770a61ddf1ac43a19bc
AnkitKumar026 <ankitkr108.live@gmail.com>
2023-08-18 17:35:50 +0530

    Add all
    calculator App program

---
+++ PracticeProgram/src/com/Kodnest/AssignmentProblem/CalculatorDilemma/CalculatorApp.java
@@ -0,0 +1,29 @@
+package com.Kodnest.AssignmentProblem.CalculatorDilemma;
+
+public class CalculatorApp {
+	void calc(char character,int num1,int num2)
+	{
+	
+	switch(character)
+	{
+	case '+':
+		System.out.println("your addition value is: "+(num1+num2));
+		break;
+	case '-':
+		System.out.println("your substraction value is: "+(num1-num2));
+		break;
+	case '*':
+		System.out.println("your multiplication value is: "+(num1*num2));
+		break;
+	case '/':
+		System.out.println("your division value is: "+(num1/num2));
+		break;
+	case '%':
+		System.out.println("your modular value is: "+(num1%num2));
+		break;
+	default :
+		System.out.println("please check your input");
+	}
+	}
+
+}
---
+++ PracticeProgram/src/com/Kodnest/AssignmentProblem/CalculatorDilemma/Calulator.java
@@ -0,0 +1,23 @@
+package com.Kodnest.AssignmentProblem.CalculatorDilemma;
+import java.util.Scanner;
+public class Calulator {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter first digit");
+		int num1=scan.nextInt();
+		System.out.println("Enter second digit");
+		int num2=scan.nextInt();
+		System.out.println("Enter a character");
+		char character=scan.next().charAt(0);
+		
+		CalculatorApp c1=new CalculatorApp();
+		
+		c1.calc(character,num1,num2);
+		scan.close();
+		
+		
+
+	}
+
+}
